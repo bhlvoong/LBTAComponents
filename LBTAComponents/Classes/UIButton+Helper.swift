8559a81e80f9d638703395fa331aaa691108d6a9
Brian Voong <bhlvoong@gmail.com>
2016-11-30 15:59:27 -0800

    better button image

--- LBTAComponents/Classes/UIButton+Helper.swift
+++ LBTAComponents/Classes/UIButton+Helper.swift
@@ -12,7 +12,8 @@ extension UIButton {
     static public func systemButton(title: String? = nil, image: UIImage? = nil, titleColor: UIColor? = .white, font: UIFont? = nil, target: Any? = nil, selector: Selector? = nil) -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle(title, for: .normal)
-        button.setImage(image, for: .normal)
+        button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
         button.setTitleColor(titleColor, for: .normal)
         button.titleLabel?.font = font
         if let selector = selector {
